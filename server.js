5eb524e6f027b7516a1ddcc94827c6093076d6ed
liliom845 <liliom.845@laposte.net>
2021-09-23 15:57:07 +0200

    Présentation de la page de saisie par défaut

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.use('/html', express.static(__dirname + "/html"));
 app.get('/', function(req, res) {
-    res.send('Yeah!')
+    //res.send('Yeah!')
+    res.redirect('/html/index.html');
 });
 
 app.get('/hello', function(req, res, next){
